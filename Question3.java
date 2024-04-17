229aee14495cbb695cd89e5799765fb7079f962d
elissalee <148605952+s10257699@users.noreply.github.com>
2024-04-18 01:37:06 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -13,7 +13,7 @@ public class Question3
     Scanner in = new Scanner(System.in);
 
     // Prompt user for integer
-    int integer = in.nextInt();
+    int integer = in.nextInt(); 
 
     // Multiply the integer by itself andd print
     int integerCalc = integer * integer;    
